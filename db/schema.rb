c12cc665c20ce842815c988d0708de66d65bb605
石井悠人” <yuto120925@outlook.jp>
2022-04-20 21:20:48 +0900

    詳細ページを修正

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,12 @@ ActiveRecord::Schema.define(version: 2022_03_10_072615) do
 
   create_table "likes", force: :cascade do |t|
     t.string "like"
+    t.integer "movie_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_likes_on_movie_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -43,4 +47,6 @@ ActiveRecord::Schema.define(version: 2022_03_10_072615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "movies"
+  add_foreign_key "likes", "users"
 end
